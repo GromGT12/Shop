76d6b39d02586da97c7cf52ddf0674e6b91f495e
GromGT12 <m.krymov@wp.pl>
2023-10-19 00:03:40 +0200

    Added Security class

--- src/main/java/com/pl/shop/service/UserServiceImpl.java
+++ src/main/java/com/pl/shop/service/UserServiceImpl.java
@@ -4,11 +4,15 @@ import com.pl.shop.dto.UserDTO;
 import com.pl.shop.repository.UserRepository;
 import com.pl.shop.shop.database.Role;
 import com.pl.shop.shop.database.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Service
@@ -27,18 +31,28 @@ public class UserServiceImpl implements UserService {
             throw new RuntimeException("Password is not equals");
         }
         User user = User.builder()
+                .name(userDTO.getUsername())
                 .password(passwordEncoder.encode(userDTO.getPassword()))
                 .email(userDTO.getEmail())
                 .role(Role.ADMIN)
                 .build();
         userRepository.save(user);
         return true;
-
-
     }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        User user = userRepository.findFirstByName(username);
+        if(user == null){
+            throw new UsernameNotFoundException("User not found with name: " + username);
+        }
+        List<GrantedAuthority> roles = new ArrayList<>();
+        roles.add(new SimpleGrantedAuthority(user.getRole().name()));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getName(),
+                user.getPassword(),
+                roles
+        );
     }
 }
